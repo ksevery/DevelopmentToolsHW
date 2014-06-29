387e0cfd2d7a7cb7f1024bf403aa52a580bd7103
ksevery <konstantinsevery@gmail.com>
2014-06-29 20:09:46 +0300

    log-tester

---
+++ ToolsTester/Test.cs
@@ -0,0 +1,24 @@
+﻿namespace ToolsTester
+{
+    using System;
+    using log4net;
+    using log4net.Appender;
+    using log4net.Config;
+
+    class Test
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Test));
+        static void Main ()
+        {
+            var fileAppender = new FileAppender();
+            fileAppender.File = "log.txt";
+            fileAppender.AppendToFile = true;
+
+            BasicConfigurator.Configure();
+
+            log.Info("Infoed.");
+            log.Error("You have erred.");
+
+        }
+    }
+}
